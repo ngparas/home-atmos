9f9022216a00fd78c7fafa15f1af49a2b1d4d946
ngparas <nickgp@gmail.com>
2018-02-25 19:17:22 -0600

    go webserver receives post requests and writes to a kakfa topic

--- producer-web/producerweb.go
+++ producer-web/producerweb.go
@@ -1,74 +1,102 @@
 package main
 
 import (
-    "encoding/json"
-    //"errors"
-    "fmt"
-    "io"
-    "io/ioutil"
-    "net/http"
-    "os"
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
 )
 
 var (
-    PORT string = os.Getenv("PORT")
+	PORT              string = os.Getenv("PORT")
+	BOOTSTRAP_SERVERS string = os.Getenv("BOOTSTRAP_SERVERS")
+	TOPIC             string = os.Getenv("SENSOR_TOPIC")
 )
 
-type SensorValue struct {
-    DeviceId string
-    SignalTime int
-    SignalName string
-    SignalValue float64
-}
-
 func isJsonContent(h http.Header) bool {
-    contentType, ok := h["Content-Type"]
-    return ok && len(contentType)==1 && contentType[0] == "application/json"
+	contentType, ok := h["Content-Type"]
+	return ok && len(contentType) == 1 && contentType[0] == "application/json"
 }
 
-func (sv *SensorValue) LoadContent(body io.ReadCloser) error {
-    bodyBytes, err := ioutil.ReadAll(body)
-    if err != nil {
-        return err
-    }
+func makeProducerHandler(producerChannel chan<- *kafka.Message) func(http.ResponseWriter, *http.Request) {
 
-    err = json.Unmarshal(bodyBytes, sv)
-    if err != nil {
-        return err
-    }
-    return nil
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			if isJsonContent(r.Header) {
+				bodyBytes, err := ioutil.ReadAll(r.Body)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+				fmt.Println(bodyBytes)
+
+				producerChannel <- &kafka.Message{
+					TopicPartition: kafka.TopicPartition{
+						Topic:     &TOPIC,
+						Partition: kafka.PartitionAny},
+					Value: bodyBytes,
+					Key:   []byte(r.RemoteAddr),
+				}
+
+				fmt.Fprintf(w, "OK")
+			}
+		} else {
+			http.Error(w, "Only POST Requests allowed", http.StatusInternalServerError)
+			return
+		}
+	}
 }
 
-func baseHandler(w http.ResponseWriter, r *http.Request) {
-
-    if r.Method == "POST" {
-
-        if isJsonContent(r.Header) {
-
-            var j SensorValue
-            err := j.LoadContent(r.Body)
-            if err != nil {
-                http.Error(w, err.Error(), http.StatusInternalServerError)
-                return
-            }
-
-            fmt.Println(j)
-            fmt.Fprintf(w, "OK")
-
-        } else {
-            http.Error(w, "Content-type must be application/json", http.StatusInternalServerError)
-            return
-        }
-
-    } else {
-        http.Error(w, "Only POST Requests are allowed", http.StatusInternalServerError)
-        return
-    }
-
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "OK")
 }
 
 func main() {
-    http.HandleFunc("/", baseHandler)
-    http.ListenAndServe(":"+PORT, nil)
 
+	time.Sleep(5 * time.Second) //sloppy but make sure kafka has time to start for now. KH would be so disappointed...
+
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": BOOTSTRAP_SERVERS})
+
+	if err != nil {
+		fmt.Printf("Failed to create producer: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Created Producer %v\n", p)
+
+	doneChan := make(chan bool)
+
+	go func() {
+		defer close(doneChan)
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				m := ev
+				if m.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", m.TopicPartition.Error)
+				} else {
+					fmt.Printf("Delivered message to topic %s [%d] at offset %v\n",
+						*m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
+				}
+				return
+
+			default:
+				fmt.Printf("Ignored event: %s\n", ev)
+			}
+		}
+	}()
+
+	// TODO - shut down properly, this isnt quite right
+	defer func() {
+		fmt.Println("Clearing events and Closing")
+		// wait for delivery report goroutine to finish
+		_ = <-doneChan
+		p.Close()
+	}()
+
+	http.HandleFunc("/", makeProducerHandler(p.ProduceChannel()))
+	http.HandleFunc("/health", healthCheck)
+
+	http.ListenAndServe(":"+PORT, nil)
 }
